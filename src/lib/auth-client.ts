e2a23fc13fa77768dc6644a0d85b892300d90142
N0RRY-20 <orywu1994@gmail.com>
2025-12-26 06:41:22 +0700

    feat: Menginisialisasi klien otentikasi dengan plugin admin dan konfigurasi peran.

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -4,7 +4,7 @@ import { ac, admin, guru, user } from "@/lib/permissions";
 
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-
+  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
   plugins: [
     adminClient({
       ac,
